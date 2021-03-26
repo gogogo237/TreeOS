1ae627ffab771cc8d6ce7fb387db5e4d9993deae
TianHappyGithub <gogogo237@163.com>
2021-03-26 14:18:43 +0800

    Enter Protected Mode
    We setup our gdt file and Entering Protected Mode.
    And we display one 'H' on the screen.

--- ExtendedProgram.asm
+++ ExtendedProgram.asm
@@ -2,14 +2,39 @@
 
 [org 0x7e00]
 
-mov bx, ExtendedSpaceSuccess
-call PrintString
+jmp EnterProtectedMode
 
-jmp $
-
+%include "gdt.asm"
 %include "print.asm"
 
-ExtendedSpaceSuccess:
-	db 'We are successfully in extended space', 0
+EnterProtectedMode:
+	call EnableA20
+	cli
+	lgdt [gdt_descriptor]
+	mov eax, cr0				; Enable Protected Mode
+	or eax, 1
+	mov cr0, eax
+	jmp codeseg:StartProtectedMode	;a long jmp, to close the CPU pipline.
+
+EnableA20:
+	in al, 0x92
+	or al, 2
+	out 0x92, al
+	ret
+
+[bits 32]
+
+StartProtectedMode:
+	
+	mov ax, dataseg
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	mov [0xb8000], byte 'H'
+
+	jmp $
 
 times 2048-($-$$) db 0
