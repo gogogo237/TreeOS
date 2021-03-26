f6e769af05dc56b98ce8b68515e1b626ae4c8d9b
德高 赖 <gogogo237@163.com>
2021-03-26 10:31:12 +0800

    Print a string of text to the screen. Function and loop are used in this program.

--- bootloader.asm
+++ bootloader.asm
@@ -1,11 +1,28 @@
+[org 0x7c00]
 
-mov ah, 0x0e    ; cursor advances after write
-mov al, 'H'     ; ASCII character to write
-int 0x10
+mov bp, 0x7c00      ; move to base pointer
+mov sp, bp          ; move to stack pointer
+
+mov bx, TestString
+call PrintString
 
 jmp $
 
+PrintString:
+    mov ah, 0x0e    ; cursor advances after write
+    .Loop:
+    cmp [bx], byte 0
+    je .Exit
+        mov al, [bx]     ; ASCII character to write
+        int 0x10        ; Display a charater on the screen
+        inc bx
+        jmp .Loop
+    .Exit:
+    ret
+
+TestString:
+    db 'Hello World! This is a test string.',0
 
 times 510-($-$$) db 0
 
-dw 0xaa55
+dw 0xaa55           ; This is a magic word for a bootloader
