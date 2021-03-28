1f4b668c738003017547dbd3b94affcc0b47f758
Arnold <gogogo237@163.com>
2021-03-28 19:07:44 +0800

    add to Linker Script

--- DiskRead.asm
+++ DiskRead.asm
@@ -1,10 +1,10 @@
 
-PROGRAM_SPACE equ 0x7e00
+PROGRAM_SPACE equ 0x8000
 
 ReadDisk:
     mov ah, 0x02                ; function request number, Read disk sectors.
     mov bx, PROGRAM_SPACE       ; ES:BX = address of user buffer. Read disk to this memory address.
-    mov al, 10                  ; number of sectors  (1-128 dec.)
+    mov al, 32                  ; number of sectors  (1-128 dec.)
     mov dl, [BOOT_DISK]         ; drive number, (0=A:, 1=2nd floppy, 80h=drive 0, 81h=drive 1)
     mov ch, 0x00                ; cylinder number  (0-1023 dec.)
     mov dh, 0x00                ; head number  (0-15 dec.)
--- compile.sh
+++ compile.sh
@@ -3,8 +3,7 @@
 nasm bootloader.asm -f bin -o bootloader.bin
 
 nasm ExtendedProgram.asm -f elf64 -o ExtendedProgram.o
-x86_64-elf-gcc -ffreestanding -mno-red-zone -m64 -c Kernel.cpp -o Kernel.o
-x86_64-elf-ld -o Kernel.tmp -Ttext 0x7e00 ExtendedProgram.o Kernel.o
-x86_64-elf-objcopy -O binary Kernel.tmp Kernel.bin
+x86_64-elf-gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c Kernel.cpp -o Kernel.o
+x86_64-elf-ld -T"link.ld"
 
 cat bootloader.bin Kernel.bin > bootloader.flp
