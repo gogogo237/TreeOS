f9f70dee42c88ae335a5b0d9380e5d9db8e74da0
Arnold <gogogo237@163.com>
2021-04-15 06:16:47 +0800

    Memory Map 2 - Print Usable Memory Regions

--- Kernel.cpp
+++ Kernel.cpp
@@ -6,18 +6,14 @@
 extern "C" void _start() {      //0x8b86
     SetCursorPosition(PositionFromCoords(0, 0)); //PositionFromCoords 0x893a SetCursorPosition 0x88c0
     InitializeIDT();                             //InitializeIDT 0x8a5a
-    
     MainKeyboardHandler = KeyboardHandler;
 
-    PrintString(IntegerToString(-500));
+    MemoryMapEntry** usableRegionMaps = GetUsableMemoryRegions();
 
-    float testFloat = -678.669f;
-
-    SetCursorPosition(PositionFromCoords(0, 1));
-    PrintString(FloatToString(testFloat, 10));
-
-    SetCursorPosition(PositionFromCoords(0, 3));
-    PrintString(IntegerToString(MemoryRegionCount));
+    for (uint_8 i = 0; i < UsableMemoryRegionCount; i++) {
+        MemoryMapEntry* memMap = usableRegionMaps[i];
+        PrintMemoryMap(memMap, CursorPosition);
+    }
 
     return;
 }
--- MemoryMap.cpp
+++ MemoryMap.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Typedefs.cpp"
+#include "TextPrint.cpp"
 
 struct MemoryMapEntry {
     uint_64 BaseAddress;
@@ -9,3 +10,43 @@ struct MemoryMapEntry {
 };
 
 extern uint_8 MemoryRegionCount;
+uint_8 UsableMemoryRegionCount;
+
+MemoryMapEntry* UsableMemoryRegions[10];
+
+void PrintMemoryMap(MemoryMapEntry* memoryMap, uint_16 position) {
+    SetCursorPosition(position);
+
+    PrintString("Memory Base: "); PrintString(IntegerToString(memoryMap->BaseAddress));
+    SetCursorPosition(position + 80);
+    PrintString("Region Length: "); PrintString(IntegerToString(memoryMap->RegionLength));
+    SetCursorPosition(position + 160);
+    PrintString("Memory Type: "); PrintString(IntegerToString(memoryMap->RegionType));
+    SetCursorPosition(position + 240);
+    PrintString("Memory Attributes: "); PrintString(HexToString(memoryMap->ExtendedAttributes));
+    SetCursorPosition(position + 400);
+}
+
+bool MemoryRegionGot = false;
+
+MemoryMapEntry** GetUsableMemoryRegions() {
+
+    if (MemoryRegionGot) {
+        return UsableMemoryRegions;
+    }
+
+    uint_8 UsableRegionIndex; 
+    for (uint_8 i = 0; i < MemoryRegionCount; i++) {
+        MemoryMapEntry* memMap = (MemoryMapEntry*)0x5000;
+        memMap += i;
+        if (memMap->RegionType == 1) {
+            UsableMemoryRegions[UsableRegionIndex] = memMap;
+            UsableRegionIndex++;
+        }
+        
+    }
+    UsableMemoryRegionCount = UsableRegionIndex;
+
+    MemoryRegionGot = true;
+    return UsableMemoryRegions;
+}
